8466dc070a4ac62871d5360c53f8459b96e3a5d1
mattshrew <mattshrewy@gmail.com>
2023-09-03 01:24:47 -0400

    finished sliders + colourblind implementation + footer

--- popup.js
+++ popup.js
@@ -1,72 +1,3 @@
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//     console.log(message);
-//     if (message.stylesheet) {
-//         const stylesheet = message.sylesheet;
-//         console.log("Received stylesheet:", stylesheet);
-//     }
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//         var currentTab = tabs[0];
-//         var tabId = currentTab.id;
-
-//         chrome.scripting.executeScript(
-//             {
-//                 target: { tabId: tabId },
-//                 function: getStyles,
-//             },
-//             function (result) {
-//                 console.log("Result:", result);
-//                 if (!chrome.runtime.lastError) {
-//                     var cssStyles = result[0].result;
-//                     console.log('Received CSS Styles in Popup:', cssStyles);
-                    
-//                     // You can now process or display the CSS styles in your popup as needed
-//                 } else {
-//                     console.error('Error executing script:', chrome.runtime.lastError);
-//                 }
-//             }
-//         );
-//     });
-// });
-  
-// function getStyles() {
-//     var styles = [];
-  
-//     var sheets = document.styleSheets;
-//     for (var i = 0; i < sheets.length; i++) {
-//       try {
-//         var rules = sheets[i].cssRules;
-//         for (var j = 0; j < rules.length; j++) {
-//           var rule = rules[j];
-//           if (rule instanceof CSSStyleRule) {
-//             styles.push(rule.cssText);
-//           }
-//         }
-//       } catch (error) {
-//         // Handle any errors due to cross-origin stylesheets
-//         console.error('Error fetching CSS rules from stylesheet:', error);
-//       }
-//     }
-  
-//     return styles;
-// }  
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     var currentTab = tabs[0];
-//     var tabId = currentTab.id;
-  
-//     const css = 'html { filter: invert(1) hue-rotate(180deg); } img, picture, video { filter: invert(1) hue-rotate(180deg); }';
-
-//     chrome.scripting.insertCSS({
-//         target: { tabId },
-//         css: css
-//     });
-// });
-
-
-
 async function setStyles() {
     chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
         var currentTab = tabs[0];
@@ -79,6 +10,8 @@ async function setStyles() {
                 target: { tabId },
                 css: result
             });
+
+            if (states.colourblind) await styleColour();
         } else {
             console.error('Error executing script:', chrome.runtime.lastError);
         }
@@ -90,52 +23,20 @@ async function getStates() {
     let states = {
         "dark": (localStorage.getItem("darkMode") == "null") ? 0.0 : 1.0,
         "night": (localStorage.getItem("nightMode") == "null") ? 0.0 : 1.0,
-        "grayscale": 0.0, // 0 to 1
-        "contrast": 1.0, // 0 to 2 (default is 1)
-        "protanopia": 0.0, // red-green or red = gray
-        "deuteranopia": 0.0, // red-green
-        "tritanopia": 0.0 // blue = gray
+        "saturation": (localStorage.getItem("saturation") == "null") ? 1.0 : localStorage.getItem("saturation")/10, // 0 to 2 (default is 1)
+        "contrast": (localStorage.getItem("contrast") == "null") ? 1.0 : localStorage.getItem("contrast")/10, // 0 to 2 (default is 1)
+        "colourblind": (localStorage.getItem("colourblind") == "null") ? 0.0 : 1.0 // red
     }
     console.log(states);
-    return states; // for now
+    return states; 
 }
 
 async function getStyles(states) {
     let css = [];
-    
-    if (states.protanopia) {
-        css.concat([
-            ""
-        ]);
-    } else {
-        css.concat([
-            ""
-        ]);
-    }
-
-    if (states.deuteranopia) {
-        css.concat([
-            ""
-        ]);
-    } else {
-        css.concat([
-            ""
-        ]);
-    }
-
-    if (states.tritanopia) {
-        css.concat([
-            ""
-        ]);
-    } else {
-        css.concat([
-            ""
-        ]);
-    }
 
     css = [
-        `html { filter: invert(${states.dark}) hue-rotate(${180*states.dark}deg) grayscale(${100*states.grayscale}%) contrast(${100*states.contrast}%) !important; }`,
-        `img, picture, video { filter: invert(${states.dark}) hue-rotate(${180*states.dark}deg) grayscale(0%) contrast(100%) !important; }`,
+        `html { filter: invert(${states.dark}) hue-rotate(${180*states.dark}deg) saturate(${100*states.saturation}%) contrast(${100*states.contrast}%) !important; }`,
+        `img, picture, video { filter: invert(${states.dark}) hue-rotate(${180*states.dark}deg) saturate(100%) contrast(100%) !important; }`,
     ];
 
     if (states.night) css.push("html::before { display: block; background: #ffffab; mix-blend-mode: multiply; content: ' '; position: fixed; top: 0; left: 0; width: 100%; height: 100vh; pointer-events: none; z-index: 999; }");
@@ -143,6 +44,18 @@ async function getStyles(states) {
 
     let res = css.join(' ')
     return res;
+}
+
+async function styleColour() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var currentTab = tabs[0];
+        var tabId = currentTab.id;
+
+        chrome.scripting.executeScript({
+            target: { tabId },
+            files: [ "stylePage.js" ]
+        });
+    });
 }
 
 
@@ -256,13 +169,7 @@ async function setAltText() {
 
 // JS For Frontend
 
-
-document.addEventListener('DOMContentLoaded', async function () {
-    await setStyles();
-    await setAltText();
-});
-
-const modes = ["master", "enableAltText", "displayAltText", "darkMode", "nightMode"];
+const modes = ["master", "darkMode", "nightMode", "enableAltText", "displayAltText", "colourblind"];
 const toggles = {"c1": "off", "c2": "on"};
 
 let master = localStorage.getItem("master");
@@ -270,8 +177,43 @@ let enableAltText = localStorage.getItem("enableAltText");
 let displayAltText = localStorage.getItem("displayAltText");
 let darkMode = localStorage.getItem("darkMode");
 let nightMode = localStorage.getItem("nightMode");
+let saturation = localStorage.getItem("saturation");
+let contrast = localStorage.getItem("contrast");
+let colourblind = localStorage.getItem("colourblind");
 
 const body = document.querySelector('body');
+
+document.addEventListener('DOMContentLoaded', async function () {
+    let saturation = localStorage.getItem("saturation");
+    let contrast = localStorage.getItem("contrast");
+    if (saturation == "null") localStorage.setItem("saturation", 10); saturation = 10;
+    if (contrast == "null") localStorage.setItem("contrast", 10); contrast = 10;
+
+    var saturation_output = document.getElementById("saturation");
+        saturation_output.innerHTML = `${saturation*10}%`;
+    var saturation_slider = document.getElementById("saturation_slider");
+        saturation_slider.value = saturation;
+        saturation_slider.addEventListener('input', function() {
+            saturation_output.innerHTML = `${saturation_slider.value*10}%`;
+            localStorage.setItem("saturation", saturation_slider.value);
+            setStyles();
+        });
+
+    var contrast_output = document.getElementById("contrast");
+        contrast_output.innerHTML = `${contrast*10}%`; 
+    var contrast_slider = document.getElementById("contrast_slider");
+        contrast_slider.value = contrast;
+        contrast_slider.addEventListener('input', function() {
+            contrast_output.innerHTML = `${contrast_slider.value*10}%`;
+            localStorage.setItem("contrast", contrast_slider.value);
+            setStyles();
+        });
+    
+
+    await setStyles();
+    await setAltText();
+});
+
 
 async function toggleMode(mode, val) {
     if (val) body.classList.add(mode);
@@ -282,9 +224,9 @@ async function toggleMode(mode, val) {
         for (const mode of modes.slice(1)) {
             toggleMode(mode, false);
         }
-    } else if (mode == "enableAltText" && val == false) {
-        // toggleMode("displayAltText", false);
-    }
+    } /* else if (mode == "enableAltText" && val == false) {
+        // toggleMode("displayAltText", false); 
+    }*/
 }
 
 for (const mode of modes) {
@@ -302,12 +244,3 @@ document.querySelectorAll(".switch-box").forEach((toggle, i) => {
         await setAltText();
     });
 });
-
-var slider = document.querySelector("slider-bar");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
